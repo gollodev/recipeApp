b45e8c870125bb56c10f2a561daff71a9692abaf
Jose Lopez <gollo_autumn@live.com>
2016-05-25 00:00:13 -0430

    Added Boilerplate App

---
+++ imports/ui/containers/RecipeContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RecipeContainer extends Component {
+	render() {
+		return (
+			<h1>RecipeContainer!</h1>
+		);
+	}
+}
+
+export default RecipeContainer;
