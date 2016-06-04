599be5f36092a3a582d52d699d60ff1df334ed5f
Jose Lopez <gollo_autumn@live.com>
2016-06-04 01:03:03 -0430

    Added RecipeList Component

---
+++ imports/ui/components/RecipeList/RecipeList.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import RecipeListItem from '../RecipeListItem/RecipeListItem';
+
+class RecipeList  extends Component {
+	render() {		
+		const { recipes } = this.props;
+		console.log(recipes);
+		return (			
+			<div className="RecipeList">
+				{ 
+					recipes.map(recipe =>
+						<RecipeListItem
+							key={recipe._id} 
+							id={recipe._id}
+							title={recipe.title}
+							description={recipe.description}
+							image={recipe.imageUrl}
+							author={recipe.author}
+						/>
+					)			
+				}
+			</div>
+		);
+	}
+}
+
+RecipeList.propTypes = {
+	recipes: PropTypes.array.isRequired
+}
+
+export default RecipeList;
