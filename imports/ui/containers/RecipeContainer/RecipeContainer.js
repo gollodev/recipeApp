13041e5e19b7b4ba2b3e1a55eca3a1fc02f02f8e
Jose Lopez <gollo_autumn@live.com>
2016-06-04 01:06:54 -0430

    Updated RecipeContainer

--- imports/ui/containers/RecipeContainer/RecipeContainer.js
+++ imports/ui/containers/RecipeContainer/RecipeContainer.js
@@ -1,22 +1,19 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import Recipes from '../../../api/collections/recipes';
 import Header from '../../components/Header/Header.js';
+import RecipeList from '../../components/RecipeList/RecipeList';
 
 class RecipeContainer extends Component {
 
-	render() {
-		console.log(this.props.recipes);		
+	render() {				
 		return (
 			<div>
 				<Header/>				
+				<RecipeList recipes={this.props.recipes}/>
 			</div>			
 		);
 	}
-}
-
-RecipeContainer.propTypes = {
-	recipes: PropTypes.array.isRequired
 }
 
 const mapCollectionsToProps = () => {
