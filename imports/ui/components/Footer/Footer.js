1e50dda81a49b3583fb8f599e740df4305743b6c
Jose Lopez <gollo_autumn@live.com>
2016-06-04 23:24:50 -0430

    Added Footer Component

---
+++ imports/ui/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+	<div className="wrapperFooter">
+		<footer className="Footer">
+			<p>RecipeApp, develop by <a href="http://joselopezweb.com" target="_blank">Jose Lopez</a></p>
+		</footer>
+	</div>	
+);
+
+export default Footer;
